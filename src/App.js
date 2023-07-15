87e9cb15f8d27a1a14550672aebde4e3b5f560f1
RiteshDB <riteshdb123@gmail.com>
2023-07-15 22:51:51 +0530

    Commit after Home page at first stage.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import Header from "./Header";
+import "./App.css";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+
+  render() {
+    const message = "Ritesh Bhandari"
+    return (
       <div>
-        <h1>This is React Project</h1>
+        <Header />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+        </div>
       </div>
+      // React.createElement('div', {id:"main-tag", className:"main-tag" },"this is a div tag")
     );
   }
 }
