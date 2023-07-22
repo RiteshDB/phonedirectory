8f36d3fb434b94844a0ba2690640706d3d50fca5
RiteshDB <riteshdb123@gmail.com>
2023-07-22 22:39:03 +0530

    Final commit on 07/22/2023

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { SubscriberCountContext } from "./SubscriberCountContext";
 
 export default function Footer(){
 
+   
+
     const count = useContext(SubscriberCountContext);
     return <h3> Number of Current Contacts: {count} </h3>
+    
 }
