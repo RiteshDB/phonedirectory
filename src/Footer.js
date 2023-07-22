6beb7f862fe12707c08ebd8442de579e8a194726
RiteshDB <riteshdb123@gmail.com>
2023-07-22 22:40:50 +0530

    final commit on 07/22/2023

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { SubscriberCountContext } from "./SubscriberCountContext";
 
 export default function Footer(){
