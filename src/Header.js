bb3bee1701d41820afa8516b7230896573758c67
RiteshDB <riteshdb123@gmail.com>
2023-07-22 22:40:19 +0530

    Final commit on 07/22/2023

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./Header.css";
 
 
-const Header = function(props){
-    return(
-        <div className="header" >
-           {props.heading}
-        </div>
+const Header = function (props) {
+
+    const [location, setLocation] = useState({ city: "", region: "", country_name: "" })
+
+    useEffect(() => {
+        fetch('https://ipapi.co/json', { method: "GET" })
+            .then(response => response.json())
+            .then(data => setLocation({ city: data.city, region: data.region, country_name: data.country_name }))
+    }, []);
+
+
+    return (
+        <Fragment>
+            <div className="header" >
+                {props.heading}
+            </div>
+            <div className="location-tag">
+                <span>Hi User, you are in {location.city}, {location.region}, {location.country_name} </span>
+            </div>
+        </Fragment>
     );
 }
 
