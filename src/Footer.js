67125514e4c965fd2645484906f7595c082894ff
RiteshDB <riteshdb123@gmail.com>
2023-07-22 15:44:22 +0530

    Integrated Back end api and Loading Back end API project

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,5 @@ import { SubscriberCountContext } from "./SubscriberCountContext";
 export default function Footer(){
 
     const count = useContext(SubscriberCountContext);
-    console.log("In footer, count is ", count);
     return <h3> Number of Current Contacts: {count} </h3>
 }
--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import AddSubscribers from "./AddSubscribers";
 import ShowSubscribers from "./ShowSubscribers";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -10,25 +10,30 @@ function PhoneDirectory() {
 
     const [subscribersList, setSubscribersList] = useState([]);
 
+    useEffect(loadData, []);
 
-    function addSubscribersHandler(newSubscriber) {
-     
-        if (subscribersList.length > 0) {
-            newSubscriber.id = ((subscribersList[subscribersList.length - 1].id) + 1);
-        }
-        else {
-            newSubscriber.id = 1;
-        }
-        console.log(subscribersList.length);
-        let tempSubscriberList = subscribersList;
-        tempSubscriberList.push(newSubscriber);
-        setSubscribersList(tempSubscriberList);
-        console.log(subscribersList.length);
+    function loadData() {
+        fetch("http://localhost:7081/contacts")
+        .then(response => response.json())
+        .then(data => setSubscribersList(data));
+
+    }
+
+    async function addSubscribersHandler(newSubscriber) {
+
+        const response = await fetch("http://localhost:7081/contacts", {method:"POST", headers: {'Content-Type': "application/json"}, body: JSON.stringify(newSubscriber) });
+        const data = await response.json();
+        loadData();
+        
     }
 
     function deleteSubscriberHandler(subscriberId) {
-        let tempSubscriberList = subscribersList.filter((subscriber) => subscriber.id !== subscriberId);
-        setSubscribersList(tempSubscriberList);
+        // let tempSubscriberList = subscribersList.filter((subscriber) => subscriber.id !== subscriberId);
+        // setSubscribersList(tempSubscriberList);
+        let url = "http://localhost:7081/contacts/"+subscriberId;
+        fetch(url, {method:"DELETE"})
+        .then(response =>response.json())
+        .then(data => loadData());
     }
 
     return (
@@ -36,7 +41,7 @@ function PhoneDirectory() {
             <Router>
                 <Routes>
                     <Route exact path='/' Component={(props) => <ShowSubscribers {...props} ShowSubscribers={subscribersList} deleteSubscriberHandler={(subscriberId) => deleteSubscriberHandler(subscriberId)} />} />
-                    <Route exact path='/Add' Component={(props) => <AddSubscribers {...props} AddSubscribersHandler={(newSubscriber) => addSubscribersHandler(newSubscriber)} /> } />
+                    <Route exact path='/Add' Component={(props) => <AddSubscribers {...props} AddSubscribersHandler={(newSubscriber) => addSubscribersHandler(newSubscriber)} />} />
                 </Routes>
             </Router>
             <SubscriberCountContext.Provider value={subscribersList.length}>
